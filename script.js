bdcd45580d7e89dd8d9e8fd9849630fa8c87577d
Navya <88023981+navysai1419@users.noreply.github.com>
2023-10-27 16:05:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your code here
+// Import the Bootstrap JavaScript components
+import 'bootstrap';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Get a reference to the button and modal
+  const openModalButton = document.getElementById('open-modal');
+  const newtonModal = document.getElementById('newton-modal');
+
+  // Add a click event listener to open the modal
+  openModalButton?.addEventListener('click', () => {
+    if (newtonModal) {
+      const modal = new bootstrap.Modal(newtonModal);
+      modal.show();
+    }
+  });
+
+  // Add an event listener to close the modal when the close button is clicked
+  newtonModal?.addEventListener('hidden.bs.modal', () => {
+    // Do something when the modal is closed (if needed)
+  });
+});
