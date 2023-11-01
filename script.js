aa146fe3467eb0d45ff234ca3a867a13242d59f9
Navya <88023981+navysai1419@users.noreply.github.com>
2023-11-01 21:45:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
-// Import the Bootstrap JavaScript components
-import 'bootstrap';
-
-document.addEventListener('DOMContentLoaded', () => {
-  // Get a reference to the button and modal
-  const openModalButton = document.getElementById('open-modal');
-  const newtonModal = document.getElementById('newton-modal');
-
-  // Add a click event listener to open the modal
-  openModalButton?.addEventListener('click', () => {
-    if (newtonModal) {
-      const modal = new bootstrap.Modal(newtonModal);
-      modal.show();
-    }
-  });
-
-  // Add an event listener to close the modal when the close button is clicked
-  newtonModal?.addEventListener('hidden.bs.modal', () => {
-    // Do something when the modal is closed (if needed)
-  });
+// script.ts
+document.getElementById("open-modal")?.addEventListener("click", () => {
+    const modal = new bootstrap.Modal(document.getElementById("newton-modal"));
+    modal.show();
+    // Add custom TypeScript logic here, e.g., handling form submissions, making AJAX requests, etc.
 });
